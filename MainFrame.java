17e4900c04af4b6e07523afd8114d51d96742369
Alex <kosymbaevalex@mail.ru>
2021-12-11 00:56:20 +0300

    Add formulas and polya(((

--- MainFrame.java
+++ MainFrame.java
@@ -21,7 +21,40 @@ import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class MainFrame extends JFrame{
-   
+    private static final int WIDTH = 400;
+    private static final int Height = 320;
+
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private JTextField textFieldZ;
+    private JTextField textForPerem;
+
+    private Double result;
+
+    private Double Perem1=0.0;
+    private Double Perem2=0.0;
+    private Double Perem3=0.0;
+
+    private JTextField textFieldResult;
+
+    private ButtonGroup radioButtons = new ButtonGroup();
+
+    private Box hboxFormulaType = Box.createHorizontalBox();
+
+    private ButtonGroup radioButtons2 = new ButtonGroup();
+    private Box peremType = Box.createHorizontalBox();
+    private JTextField textFieldPerem;
+
+    private int formulaId = 1;
+    private int PeremId=1;
+
+    public Double calculate1(Double x, Double y, Double z){
+        return Math.pow(Math.cos(Math.pow(Math.E,x)+Math.pow(Math.exp(1+y),2)+Math.sqrt(Math.pow(Math.E,Math.cos(x))+Math.pow(Math.sin(z*Math.PI),2))+Math.sqrt(1/x)+Math.cos(Math.pow(y,2))),Math.sin(z));
+    }
+    public Double calculate2(Double x, Double y, Double z){
+
+        return (1+Math.sqrt(z*x))/(Math.pow(1+Math.pow(x,3),1/y));
+    }
     // Главный метод класса
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
