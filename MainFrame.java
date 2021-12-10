1301ce1fcf8cb4066bd12ec3683672efd742f97e
Alex <kosymbaevalex@mail.ru>
2021-12-11 00:54:24 +0300

    Start to do

---
+++ MainFrame.java
@@ -0,0 +1,33 @@
+package bsu.rfe.java.group10.lab2.Kosymbaev.varB12;
+
+import java.awt.BorderLayout;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class MainFrame extends JFrame{
+   
+    // Главный метод класса
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(300,300);
+    }
+}
+
